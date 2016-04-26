d4181c2540c7ee4c9d5d6ee49a81eb7f90016bd5
Sandro Fasser <Sandro Fasser@Jrg>
2016-04-26 16:45:30 +0200

    Hoffa es funktioniert oder i töta github

---
+++ src/testsandro/Finanzkalkulator.java
@@ -0,0 +1,56 @@
+package testsandro;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Finanzkalkulator extends UnicastRemoteObject implements IFinanceCalc{
+    
+    protected Finanzkalkulator() throws RemoteException {
+		
+	}
+	/**
+     * Berechnet das Endkapital.
+     * @param startKapital double
+     * @param zinssatz double
+     * @param anzahlJahre double
+     * @return endkapital double
+     * @throws Exception
+     */
+	public double getEndkapital(double startKapital, double zinssatz,
+			double anzahlJahre) throws RemoteException {
+
+		double q = 1 + zinssatz;
+		return startKapital * Math.pow(q, anzahlJahre);
+	}
+	/**
+     * Berechnet den Barwert.
+     * @param endKapital double
+     * @param zinssatz double
+     * @param anzahlJahre double
+     * @return barwert double
+     * @throws Exception
+     */
+	public double getBarwert(double endKapital, double zinssatz,
+			double anzahlJahre) throws RemoteException {
+
+		double q = 1 + zinssatz;
+		return endKapital * Math.pow(q, ((-1) * anzahlJahre));
+	}
+
+
+    /**
+     * Berechnet die Anuität.
+     * @param kapital double
+     * @param zinssatz double
+     * @param anzahlJahre double
+     * @return anuitaet double
+     * @throws Exception
+     */
+	public double getAnuitaet(double kapital, double zinssatz,
+			double anzahlJahre) throws RemoteException {
+
+		double q = 1 + zinssatz/100;
+        return kapital * (Math.pow(q, anzahlJahre) * (q - 1))
+                / (Math.pow(q, anzahlJahre) - 1);
+	}
+}
